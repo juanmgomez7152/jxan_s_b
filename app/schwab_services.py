b39774df699f9d596181128d1bd47ffdce6f53db
Juan Gomez Guzman (LAPTOP) <juan.m.gomez7152@gmail.com>
2025-05-05 09:55:14 -0500

    Feat: Add option chain retrieval functionality and update controller logic

--- app/schwab_services.py
+++ app/schwab_services.py
@@ -1,5 +1,6 @@
 from app.schwabdev.client import Client as SchwabClient
 from app.models.core_quote_model import CoreQuoteModel
+from app.models.option_and_chain_model import OptionContract, OptionChainSnapshot
 from dotenv import load_dotenv
 from datetime import datetime
 import logging
@@ -28,9 +29,8 @@ def controller_schwab():
                 list_of_tickers = ["MSFT", "AAPL", "GOOGL"]
                 get_core_quote(list_of_tickers)
             case "3":
-                account_id = input("Enter account ID: ")
-                positions = get_schwab_positions(account_id)
-                print(positions)
+                tickers = ["AAPL"]
+                get_options_chain(tickers)
             case "x":
                 print("Exiting...")
                 terminar = False
@@ -59,6 +59,38 @@ def get_core_quote(tickers=[]):
         quote_list.append(_parse_quote(json_response, ticker))
         
     return quote_list
+
+def get_options_chain(tickers=[], strike_prices = []):#Change strike_prices to a Dict with the ticker as key and a list of strike prices as value
+    options_chain_list = []
+    for ticker in tickers:
+        for str_pr in strike_prices:
+            response = schwab_client.option_chains(ticker, str_pr)
+            json_response = response.json()
+            options_chain = OptionChainSnapshot(
+                expiration_date=json_response['expirationDate'],
+                options=[OptionContract(
+                    contract_symbol=option['contractSymbol'],
+                    strike_price=option['strikePrice'],
+                    type=option['type'],
+                    bid=option['bidPrice'],
+                    ask=option['askPrice'],
+                    last=option['lastPrice'],
+                    open_interest=option['openInterest'],
+                    volume=option['volume'],
+                    implied_volatility=option['impliedVolatility'],
+                    delta=option['delta'],
+                    gamma=option['gamma'],
+                    theta=option['theta'],
+                    vega=option['vega']
+                ) for option in json_response['options']]
+            )
+            options_chain_list.append(options_chain)
+
+    
+    return options_chain_list
+
+def get_options(ticker):
+    pass
 
 def _parse_quote(json_response, ticker):
     try:
